c4fe9c8ad6e3d0dd96726ca689c86bbb0cdf1f06
Martin Hanusch <martin.hanusch@let.ethz.ch>
2019-09-30 09:15:44 +0200

    Update of version.php
    This update of version.php refers to commit 30c2e10 from 27.09.2019.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_sc';
-$plugin->version = 2019092500;
+$plugin->version = 2019092600;
 $plugin->requires = 2017051501; // Moodle >= 3.3.
 
 $plugin->maturity = MATURITY_STABLE;
