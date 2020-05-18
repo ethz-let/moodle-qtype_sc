ce7238e64bc68b2b20b511bee9a4b35ed2dc51fd
Martin Hanusch <martin.hanusch@let.ethz.ch>
2020-05-18 13:46:25 +0200

    upgrading thirdpartylibs.xml
    - Deleted thirdpartylibs.xml since there are no longer thirdparty libs in use

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_sc';
-$plugin->version = 2020051801;
+$plugin->version = 2020051802;
 $plugin->requires = 2017051501; // Moodle >= 3.3.
 
 $plugin->maturity = MATURITY_STABLE;
