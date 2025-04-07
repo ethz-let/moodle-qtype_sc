390a391cbcd1a95e7f5948cc2a90b16989bfeaf8
Nexterday <nexterday@users.noreply.github.com>
2025-04-07 10:27:34 +0200

    Fix duplicates in restore

--- backup/moodle2/restore_qtype_sc_plugin.class.php
+++ backup/moodle2/restore_qtype_sc_plugin.class.php
@@ -165,4 +165,52 @@ class restore_qtype_sc_plugin extends restore_qtype_plugin {
 
         return $contents;
     }
+        /**
+     * Convert the backup structure of the SC question type into a structure matching its
+     * question data. This data will then be used to produce an identity hash for comparison with
+     * questions in the database. We have to override the parent function, because we use a special
+     * structure during backup.
+     *
+     * @param array $backupdata
+     * @return stdClass
+     */
+    public static function convert_backup_to_questiondata(array $backupdata): stdClass {
+        // First, convert standard data via the parent function.
+        $questiondata = parent::convert_backup_to_questiondata($backupdata);
+        /**
+        * Convert the row data. An array of all rows (as objects) is stored in $questiondata->options.
+        * Furthermore, there is the property $questiondata->option which contains an array of objects
+        * with the properties text (= row's optiontext field) and format (= row's optiontextformat field).
+        * And finally, there is the property $questiondata->feedback containing an array of objects with
+        * the properties text (= row's optionfeedback field) and format (= row's optionfeedbackformat field).
+        */
+        $questiondata->option = [];
+        $questiondata->feedback = [];
+        foreach ($backupdata['plugin_qtype_sc_question']['rows']['row'] as $row) {
+            $questiondata->options->rows[] = (object) $row;
+            $questiondata->option[] = (object)[
+                'text' => $row['optiontext'],
+                'format' => $row['optiontextformat'],
+            ];
+            $questiondata->feedback[] = (object)[
+                'text' => $row['optionfeedback'],
+                'format' => $row['optionfeedbackformat'],
+            ];
+        }
+
+        return $questiondata;
+    }
+
+    /**
+     * Return a list of paths to fields to be removed from questiondata before creating an identity hash.
+     * We have to remove the id and questionid property from all rows, columns and weights.
+     *
+     * @return array
+     */
+    protected function define_excluded_identity_hash_fields(): array {
+        return [
+            '/options/rows/id',
+            '/options/rows/questionid',
+        ];
+    }
 }
